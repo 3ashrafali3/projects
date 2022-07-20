2dd2920f869697fd7aacccaba449a436c9b01d1b
Malagi <ashraf-ali.malagi@capgemini.com>
2022-07-20 15:30:02 +0200

    added file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bismillah, Hello Ashraf.
+          Bismillah, Hello Ashraf..
         </p>
         <a
           className="App-link"
